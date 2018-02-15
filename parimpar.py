c0953c4b96ddd7e58d6f83db1ee1ec7ade9c23fd
FERfecto <35691475+FERfecto@users.noreply.github.com>
2018-02-15 13:27:10 +0100

    Update parimpar.py

--- parimpar.py
+++ parimpar.py
@@ -2,10 +2,10 @@
 
 print("PARES E IMPARES")
 num1 = int(input("Escriba un número entero: "))
-num2 = int(input("Escriba un número entero mayor o igual que {numero_1}: "))
+num2 = int(input("Escriba un número entero mayor o igual que {num1}: "))
 
 if num2 < num1:
-    print("¡Le he pedido un número entero mayor o igual que {numero_1}!")
+    print("¡Le he pedido un número entero mayor o igual que {num1}!")
 else:
     for i in range(num1, num2 + 1):
         if i % 2 == 0:
